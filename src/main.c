e9dcab7c3f38d4594550efe587a7af7a2820a804
dariocastillo11 <dario.4979102@gmail.com>
2024-10-25 13:26:13 -0300

    v5

--- src/main.c
+++ src/main.c
@@ -2,44 +2,39 @@
  * @file main.c
  * @brief Entry point of the system
  */
-//STD headers
+// STD headers
 #include <stdio.h>
 #include <stdlib.h>
 
-//LiB or deps headers
+// LiB or deps headers
 #include <cjson/cJSON.h>
 
-
-//SAME FOLDER HEADERS,WiTH
-
+// SAME FOLDER HEADERS,WiTH
 
 int main(void)
 {
-    //create a json obj
+    // create a json obj
     cJSON* root = cJSON_CreateObject();
 
-   //add samedate
-   cJSON_AddStringToObject(root,"name","dario");
-   cJSON_AddNumberToObject(root,"age",25);
-   cJSON_AddBoolToObject(root,"is_student",0);
+    // add samedate
+    cJSON_AddStringToObject(root, "name", "dario");
+    cJSON_AddNumberToObject(root, "age", 25);
+    cJSON_AddBoolToObject(root, "is_student", 0);
 
-   //complex date types
-   cJSON* hobbies= cJSON_CreateArray();
+    // complex date types
+    cJSON* hobbies = cJSON_CreateArray();
 
-   cJSON_AddItemToArray(hobbies,cJSON_CreateString("reading"));
-   cJSON_AddItemToArray(hobbies,cJSON_CreateString("traning"));
-   cJSON_AddItemToObject(root,"hobbies",hobbies);
+    cJSON_AddItemToArray(hobbies, cJSON_CreateString("reading"));
+    cJSON_AddItemToArray(hobbies, cJSON_CreateString("traning"));
+    cJSON_AddItemToObject(root, "hobbies", hobbies);
 
+    // print obj
+    char* json_string = cJSON_Print(root);
+    printf("%s\n", json_string);
 
-   //print obj
-   char* json_string = cJSON_Print(root);
-   printf("%s\n", json_string);
-
-   //cleanup
-   cJSON_Delete(root);
-   free(json_string);
-
+    // cleanup
+    cJSON_Delete(root);
+    free(json_string);
 
     return 0;
-
 }
